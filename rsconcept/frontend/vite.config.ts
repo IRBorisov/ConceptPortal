cd96ad227465e019f2061815041a889d8581981c
IRBorisov <8611739+IRBorisov@users.noreply.github.com>
2023-08-09 16:55:53 +0300

    Setup https for prod

--- rsconcept/frontend/vite.config.ts
+++ rsconcept/frontend/vite.config.ts
@@ -1,14 +1,10 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    // https: {
-    //   key: fs.readFileSync('cert/portal-key.pem'),
-    //   cert: fs.readFileSync('cert/portal-cert.pem'),
-    // }
+    port: 3000
   }
 })
