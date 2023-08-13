d9fbb57fc6b246166b24c0671b56c9947b5959a0
IRBorisov <8611739+IRBorisov@users.noreply.github.com>
2023-08-13 23:42:48 +0300

    Revert "Improve chunks"
    This reverts commit 12fb56e9019ae6426d776018c9e29db751f7f841.

--- rsconcept/frontend/vite.config.ts
+++ rsconcept/frontend/vite.config.ts
@@ -6,22 +6,5 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000
-  },
-  build: {
-    chunkSizeWarningLimit: 1024, // KB
-    rollupOptions: {
-      output: {
-        manualChunks:
-        (id) => {
-          if (id.includes('@codemirror') || id.includes('@uiw') || id.includes('@lezer/lr')) return 'parsing';
-          if (id.includes('reagraph') || id.includes('graphology')) return 'graph';
-          if (id.includes('react-data-table-component')) return 'data-table';
-          if (id.includes('@react-three')) return 'react-three';
-          if (id.includes('three') || id.includes('camera')) return 'graphics';
-          if (id.includes('node_modules')) return 'imports';
-          return 'index';
-        },
-      },
-    },
   }
 })
