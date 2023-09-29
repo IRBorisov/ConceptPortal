70737410c2631a164a1cd7d901685cd6cbd0806f
IRBorisov <8611739+IRBorisov@users.noreply.github.com>
2023-09-29 16:28:26 +0300

    Minor UI fixes

--- rsconcept/frontend/src/components/Help/HelpRSFormItems.tsx
+++ rsconcept/frontend/src/components/Help/HelpRSFormItems.tsx
@@ -3,17 +3,16 @@ import InfoCstStatus from './InfoCstStatus';
 
 function HelpRSFormItems() {
   return (
-    <div>
-      <h1>Горячие клавиши</h1>
-      <p><b>Двойной клик / Alt + клик</b> - редактирование конституенты</p>
-      <p><b>Клик на квадрат слева</b> - выделение конституенты</p>
-      <p><b>Alt + вверх/вниз</b> - движение конституент</p>
-      <p><b>Delete</b> - удаление конституент</p>
-      <p><b>Alt + 1-6,Q,W</b> - добавление конституент</p>
-      <Divider margins='mt-2' />
-      <InfoCstStatus title='Статусы' />
-    </div>
-  );
+  <div>
+    <h1>Горячие клавиши</h1>
+    <p><b>Двойной клик / Alt + клик</b> - редактирование конституенты</p>
+    <p><b>Клик на квадрат слева</b> - выделение конституенты</p>
+    <p><b>Alt + вверх/вниз</b> - движение конституент</p>
+    <p><b>Delete</b> - удаление конституент</p>
+    <p><b>Alt + 1-6,Q,W</b> - добавление конституент</p>
+    <Divider margins='mt-2' />
+    <InfoCstStatus title='Статусы' />
+  </div>);
 }
 
 export default HelpRSFormItems;
--- rsconcept/frontend/src/components/Help/HelpRSLang.tsx
+++ rsconcept/frontend/src/components/Help/HelpRSLang.tsx
@@ -4,7 +4,7 @@ import useWindowSize from '../../hooks/useWindowSize';
 import { urls, youtube } from '../../utils/constants';
 import EmbedYoutube from '../Common/EmbedYoutube';
 
-const OPT_VIDEO_H = 1080
+const OPT_VIDEO_H = 1080;
 
 function HelpRSLang() {
   const windowSize = useWindowSize();
@@ -17,26 +17,25 @@ function HelpRSLang() {
   }, [windowSize]);
 
   return (
-    <div className='flex flex-col w-full gap-4'>
-      <div>
-        <h1>Родоструктурная экспликация концептуальных схем</h1>
-        <p>Формальная запись (<i>экспликация</i>) концептуальных схем осуществляется с помощью языка родов структур.</p>
-        <p>Данный математический аппарат основан на аксиоматической теории множеств Цермелло-Френкеля и аппарате родов структур Н.Бурбаки.</p>
-        <p>Для ознакомления с основами родов структур можно использовать следующие материалы:</p>
-        <ul>
-          <li>1. <a className='underline' href={urls.intro_video}>Видео: Краткое введение в мат. аппарат</a></li>
-          <li>2. <a className='underline' href={urls.ponomarev}>Текст: Учебник И. Н. Пономарева</a></li>
-          <li>3. <a className='underline' href={urls.full_course}>Видео: лекции для 4 курса (второй семестр 2022-23 год)</a></li>
-        </ul>
-      </div>
-      <div className='justify-center hidden w-full md:flex fleex-col'>
-        <EmbedYoutube
-          videoID={youtube.intro}
-          pxHeight={videoHeight}
-        />
-      </div>
+  <div className='flex flex-col w-full gap-4'>
+    <div>
+      <h1>Родоструктурная экспликация концептуальных схем</h1>
+      <p>Формальная запись (<i>экспликация</i>) концептуальных схем осуществляется с помощью языка родов структур.</p>
+      <p>Данный математический аппарат основан на аксиоматической теории множеств Цермелло-Френкеля и аппарате родов структур Н.Бурбаки.</p>
+      <p>Для ознакомления с основами родов структур можно использовать следующие материалы:</p>
+      <ul>
+        <li>1. <a className='underline' href={urls.intro_video}>Видео: Краткое введение в мат. аппарат</a></li>
+        <li>2. <a className='underline' href={urls.ponomarev}>Текст: Учебник И. Н. Пономарева</a></li>
+        <li>3. <a className='underline' href={urls.full_course}>Видео: лекции для 4 курса (второй семестр 2022-23 год)</a></li>
+      </ul>
     </div>
-  );
+    <div className='justify-center hidden w-full md:flex fleex-col'>
+    <EmbedYoutube
+      videoID={youtube.intro}
+      pxHeight={videoHeight}
+    />
+    </div>
+  </div>);
 }
 
 export default HelpRSLang;
