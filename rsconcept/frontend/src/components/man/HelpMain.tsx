435411eba76f78a2a4bb3f48b3c71e6e55f6a15f
IRBorisov <8611739+IRBorisov@users.noreply.github.com>
2024-05-06 18:41:25 +0300

    Update helper pages

--- rsconcept/frontend/src/components/man/HelpMain.tsx
+++ rsconcept/frontend/src/components/man/HelpMain.tsx
@@ -1,4 +1,6 @@
+import { urls } from '@/app/urls';
 import TextURL from '@/components/ui/TextURL';
+import { HelpTopic } from '@/models/miscellaneous';
 import { external_urls } from '@/utils/constants';
 
 function HelpMain() {
@@ -7,16 +9,28 @@ function HelpMain() {
   <div>
     <h1>Портал</h1>
     <p>Портал позволяет анализировать предметные области, формально записывать системы определений и синтезировать их с помощью математического аппарата родов структур</p>
+    <br/>
+
     <h2>Основные разделы</h2>
-    <li><TextURL text='Библиотека' href='/library' /> - библиотека концептуальных схем</li>
-    <li><TextURL text='Профиль' href='/profile' /> - данные пользователя и смена пароля</li>
+    <li><TextURL text='Библиотека' href={urls.library} /> - библиотека концептуальных схем</li>
+    <li><TextURL text='Справка' href={urls.manuals} /> - справочные материалы</li>
+    <li><TextURL text='Профиль' href={urls.profile} /> - данные пользователя и смена пароля</li>
+    <br/>
     
-    <h2>Навигация</h2>
-    <p>Навигационную панель можно скрыть с помощью кнопки в правом верхнем углу</p>
-    <p>В меню пользователя (правый угол) доступно редактирование пользователя и изменение цветовой темы</p>
+    <h2>Навигация и настройки</h2>
+    <li>Навигационную панель можно скрыть с помощью кнопки в правом верхнем углу</li>
+    <li>В меню пользователя доступен ряд настроек и управление активным профилем</li>
+    <br/>
+
+    <h2>Лицензирование и раскрытие информации</h2>
+    <li>Пользователи Портала сохраняют авторские права на создаваемый ими контент</li>
+    <li>Политика обработки данных доступна по <TextURL text='ссылке' href={urls.help_topic(HelpTopic.PRIVACY)} /></li>
+    <li>Портал является проектом с открытым исходным кодом, доступным на <TextURL text='Github' href={external_urls.git_repo}/></li>
+    <li>Данный сайт использует доменное имя и серверные мощности <TextURL text='Центра Концепт' href={external_urls.concept}/></li>
+    <br/>
 
     <h2>Поддержка</h2>
-    <p>Портал разрабатывается <TextURL text='Центром Концепт' href={external_urls.concept}/> и является проектом с открытым исходным кодом, доступным на <TextURL text='Github' href={external_urls.git_repo}/></p>
+    <p>Портал разрабатывается <TextURL text='Центром Концепт' href={external_urls.concept}/></p>
     <p>Ваши пожелания по доработке, найденные ошибки и иные предложения можно направлять по email: <TextURL href={external_urls.mail_portal} text='portal@acconcept.ru'/></p>
   </div>);
 }
