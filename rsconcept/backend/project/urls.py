8f855ab7df7d86c0a7a1b2fb37e9a3c12e51a1ed
IRBorisov <8611739+IRBorisov@users.noreply.github.com>
2024-06-05 19:20:44 +0300

    Add robots.txt for REST API

--- rsconcept/backend/project/urls.py
+++ rsconcept/backend/project/urls.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import TemplateView
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
@@ -11,5 +12,9 @@ urlpatterns = [
     path('users/', include('apps.users.urls')),
     path('schema', SpectacularAPIView.as_view(), name='schema'),
     path('redoc', SpectacularRedocView.as_view()),
+    path(
+        'robots.txt',
+        TemplateView.as_view(template_name='robots.txt', content_type='text/plain'),
+    ),
     path('', SpectacularSwaggerView.as_view(), name='home'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
