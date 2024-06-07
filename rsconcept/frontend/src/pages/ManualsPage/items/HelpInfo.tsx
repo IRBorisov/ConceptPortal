186545afe931d4d30fed185100a7ff3322002592
IRBorisov <8611739+IRBorisov@users.noreply.github.com>
2024-06-07 15:35:34 +0300

    Add rules of conduct

--- rsconcept/frontend/src/pages/ManualsPage/items/HelpInfo.tsx
+++ rsconcept/frontend/src/pages/ManualsPage/items/HelpInfo.tsx
@@ -5,8 +5,11 @@ import Subtopics from '../Subtopics';
 function HelpInfo() {
   return (
     <div>
-      <h1>Документы</h1>
-      <p>TBD.</p>
+      <h1>Справочная информация и документы</h1>
+      <p>
+        Раздел содержит различные документы, задающие правовой статус Портала,
+        <br />а также документацию для разработчиков.
+      </p>
 
       <Subtopics headTopic={HelpTopic.INFO} />
     </div>
