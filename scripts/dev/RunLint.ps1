5682da57845c4868b3984c70452de4cebe57f3a4
Ivan <8611739+IRBorisov@users.noreply.github.com>
2024-11-20 21:40:59 +0300

    R: Add frontend linter to script

--- scripts/dev/RunLint.ps1
+++ scripts/dev/RunLint.ps1
@@ -1,11 +1,14 @@
-# Run coverage analysis
+# Run linters
+
 $backend = Resolve-Path -Path "$PSScriptRoot\..\..\rsconcept\backend"
+$frontend = Resolve-Path -Path "$PSScriptRoot\..\..\rsconcept\frontend"
 
 function RunLinters() {
-  BackendLint
+  LintBackend
+  LintFrontend
 }
 
-function BackendLint() {
+function LintBackend() {
   $pylint = "$backend\venv\Scripts\pylint.exe"
   $mypy = "$backend\venv\Scripts\mypy.exe"
 
@@ -13,6 +16,11 @@ function BackendLint() {
   $env:DJANGO_SETTINGS_MODULE = "project.settings"
   & $pylint project apps
   & $mypy project apps
+}
+
+function LintFrontend() {
+  Set-Location $frontend
+  & npm run lint
 }
 
 RunLinters
