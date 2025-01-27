da9745125e8d48df902c1a1071df4bbcfc68782d
Ivan <8611739+IRBorisov@users.noreply.github.com>
2025-01-27 16:12:41 +0300

    T: Implement backend mocking for tests

--- rsconcept/frontend/tests/app.spec.ts
+++ rsconcept/frontend/tests/app.spec.ts
@@ -1,9 +1,14 @@
 import { expect, test } from '@playwright/test';
 
+import { authAnonymous } from './mocks/auth';
+
 test('should load the homepage and display login button', async ({ page }) => {
+  await authAnonymous(page);
+
   await page.goto('/');
   await expect(page).toHaveTitle('Концепт Портал');
   await expect(page.getByRole('heading', { name: 'Портал' })).toBeVisible();
+
   await page.click('.h-full > .mr-1');
   await expect(page.getByText('Логин или email')).toBeVisible();
 });
---
+++ rsconcept/frontend/tests/mocks/auth.ts
@@ -0,0 +1,9 @@
+import { Page } from '@playwright/test';
+
+import { BACKEND_URL } from '../constants';
+
+export async function authAnonymous(page: Page) {
+  await page.route(`${BACKEND_URL}/users/api/auth`, async route => {
+    await route.fulfill({ json: { id: null } });
+  });
+}
