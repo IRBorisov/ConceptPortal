9c0315f049396c9ece8548bc7c7fc9bfbfae7abc
Ivan <8611739+IRBorisov@users.noreply.github.com>
2025-02-20 14:59:52 +0300

    F: Disable react-scan for production

--- rsconcept/frontend/src/main.tsx
+++ rsconcept/frontend/src/main.tsx
@@ -1,9 +1,16 @@
 import { createRoot } from 'react-dom/client';
+import { scan } from 'react-scan';
 
 import { GlobalProviders } from './app/GlobalProviders';
 import { App } from './app';
 
 import './index.css';
+
+if (typeof window !== 'undefined') {
+  scan({
+    enabled: !!process.env.NODE_ENV && process.env.NODE_ENV === 'development'
+  });
+}
 
 createRoot(document.getElementById('root')!).render(
   <GlobalProviders>
