418fbdf1953a2e6386cd91b1bcdcacb18906f74d
Ivan <8611739+IRBorisov@users.noreply.github.com>
2025-02-22 11:23:11 +0300

    B: Fix tabIndex for modals

--- rsconcept/frontend/src/app/ApplicationLayout.tsx
+++ rsconcept/frontend/src/app/ApplicationLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet } from 'react-router';
 
 import { ModalLoader } from '@/components/Modal';
 import { useAppLayoutStore, useMainHeight, useViewportHeight } from '@/stores/appLayout';
+import { useDialogsStore } from '@/stores/dialogs';
 
 import { NavigationState } from './Navigation/NavigationContext';
 import { Footer } from './Footer';
@@ -20,6 +21,7 @@ export function ApplicationLayout() {
   const noNavigationAnimation = useAppLayoutStore(state => state.noNavigationAnimation);
   const noNavigation = useAppLayoutStore(state => state.noNavigation);
   const noFooter = useAppLayoutStore(state => state.noFooter);
+  const activeDialog = useDialogsStore(state => state.active);
 
   return (
     <NavigationState>
@@ -39,7 +41,11 @@ export function ApplicationLayout() {
 
         <Navigation />
 
-        <div className='overflow-x-auto max-w-[100vw]' style={{ maxHeight: viewportHeight }}>
+        <div
+          className='overflow-x-auto max-w-[100vw]'
+          style={{ maxHeight: viewportHeight }}
+          inert={activeDialog !== null}
+        >
           <main className='cc-scroll-y' style={{ overflowY: showScroll ? 'scroll' : 'auto', minHeight: mainHeight }}>
             <GlobalLoader />
             <MutationErrors />
