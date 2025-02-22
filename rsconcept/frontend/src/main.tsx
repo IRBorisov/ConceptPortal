1afc179d06a05dfe905c422194c85c3a85068814
Ivan <8611739+IRBorisov@users.noreply.github.com>
2025-02-22 14:29:38 +0300

    R: make react-scan import optional

--- rsconcept/frontend/src/main.tsx
+++ rsconcept/frontend/src/main.tsx
@@ -1,15 +1,16 @@
 import { createRoot } from 'react-dom/client';
-import { scan } from 'react-scan';
 
 import { GlobalProviders } from './app/GlobalProviders';
 import { App } from './app';
 
 import './index.css';
 
-if (typeof window !== 'undefined') {
-  scan({
-    enabled: !!process.env.NODE_ENV && process.env.NODE_ENV === 'development'
-  });
+if (typeof window !== 'undefined' && !!process.env.NODE_ENV && process.env.NODE_ENV === 'development') {
+  void import('react-scan').then(module =>
+    module.scan({
+      enabled: true
+    })
+  );
 }
 
 createRoot(document.getElementById('root')!).render(
