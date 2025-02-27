6d5466dd754c28aee9d5bdbd71aedf2ca5c7b963
Ivan <8611739+IRBorisov@users.noreply.github.com>
2025-02-27 14:01:02 +0300

    M: Fix height for tooltip

--- rsconcept/frontend/src/features/oss/components/OperationTooltip.tsx
+++ rsconcept/frontend/src/features/oss/components/OperationTooltip.tsx
@@ -9,9 +9,11 @@ export function OperationTooltip() {
   const hoverOperation = useOperationTooltipStore(state => state.activeOperation);
   return (
     <Tooltip
+      clickable
       id={globalIDs.operation_tooltip}
       layer='z-topmost'
-      className='max-w-[35rem] max-h-[40rem] dense'
+      className='max-w-[35rem] dense'
+      style={{ maxHeight: '30rem', overflowY: 'auto' }}
       hidden={!hoverOperation}
     >
       {hoverOperation ? <InfoOperation operation={hoverOperation} /> : null}
