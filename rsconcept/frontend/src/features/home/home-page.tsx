191d73e60ed4f7b6ad84a4e2f503310c21ee4c85
Ivan <8611739+IRBorisov@users.noreply.github.com>
2025-03-13 20:21:16 +0300

    B: Fix redirect on first load

--- rsconcept/frontend/src/features/home/home-page.tsx
+++ rsconcept/frontend/src/features/home/home-page.tsx
@@ -1,14 +1,17 @@
 import { urls, useConceptNavigation } from '@/app';
 import { useAuthSuspense } from '@/features/auth';
 
+import { PARAMETER } from '@/utils/constants';
+
 export function HomePage() {
   const router = useConceptNavigation();
   const { isAnonymous } = useAuthSuspense();
 
   if (isAnonymous) {
-    router.replace({ path: urls.manuals });
+    // Note: Timeout is needed to let router initialize
+    setTimeout(() => router.replace({ path: urls.login }), PARAMETER.minimalTimeout);
   } else {
-    router.replace({ path: urls.library });
+    setTimeout(() => router.replace({ path: urls.library }), PARAMETER.minimalTimeout);
   }
 
   return null;
