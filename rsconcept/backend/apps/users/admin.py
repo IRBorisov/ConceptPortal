79158c5cf5a2e512d5ee84ee05ed0603c337c841
Ivan <8611739+IRBorisov@users.noreply.github.com>
2025-03-19 12:08:41 +0300

    F: Improve admin panel for User

--- rsconcept/backend/apps/users/admin.py
+++ rsconcept/backend/apps/users/admin.py
@@ -1,1 +1,26 @@
 ''' Admin: User profile and Authorization. '''
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets
+    list_display = (
+        'username',
+        'email',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active',
+        'date_joined',
+        'last_login')
+    ordering = ['date_joined', 'username']
+    search_fields = ['email', 'first_name', 'last_name', 'username']
+    list_filter = ['is_staff', 'is_superuser', 'is_active']
+
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
