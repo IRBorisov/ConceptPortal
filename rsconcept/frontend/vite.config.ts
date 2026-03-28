d555c54a32e61d720ae2d41fef5e1f8479555902
Ivan <8611739+IRBorisov@users.noreply.github.com>
2026-03-28 22:23:07 +0300

    R: Simplify building config

--- rsconcept/frontend/vite.config.ts
+++ rsconcept/frontend/vite.config.ts
@@ -1,138 +1,43 @@
-import path from 'path';
-
 import babel from '@rolldown/plugin-babel';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
-import { defineConfig, loadEnv, type PluginOption } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { visualizer } from "rollup-plugin-visualizer";
+import { fileURLToPath } from 'url';
 
-import { dependencies } from './package.json';
-
-// Packages to include in main app bundle
-const inlinePackages = [
-  'react',
-  'react-router',
-  'react-dom',
-  'react-icons',
-  'react-hook-form',
-  'react-tooltip',
-  'react-toastify',
-
-  'global',
-  'react-scan',
-
-  'axios',
-  'zod',
-  'zustand',
-  '@tanstack/react-query',
-  '@hookform/resolvers',
-
-  'class-variance-authority',
-  'clsx',
-  'use-debounce',
-  'tailwind-merge'
-];
-
-// Rollup warnings that should not be displayed (not used in Vitest)
-const isVitest = process.env.VITEST === 'true' || process.env.NODE_ENV === 'test';
-const warningsToIgnore = !isVitest
-  ? [
-    // ======== Obsolete ========
-    // ['SOURCEMAP_ERROR', "Can't resolve original location of error"],
-    // ['MODULE_LEVEL_DIRECTIVE', 'Module level directives cause errors when bundled']
-  ]
-  : [];
-
-// https://vitejs.dev/config/
 export default ({ mode }: { mode: string; }) => {
-  process.env = {
-    ...process.env,
-    ...loadEnv(mode, process.cwd())
-  };
+  const env = loadEnv(mode, process.cwd(), '');
   return defineConfig({
     appType: 'spa',
 
     plugins: [
       tailwindcss(),
-      visualizer({
-        filename: 'stats.html',
-        emitFile: true,
-        template: 'treemap'
-      }),
-
       react(),
       babel({
         presets: [reactCompilerPreset()]
       }),
-      muteWarningsPlugin(warningsToIgnore)
+
+      mode === 'analyze' && visualizer({
+        filename: 'stats.html',
+        template: 'treemap'
+      })
     ],
     server: {
-      port: Number(process.env.VITE_PORTAL_FRONT_PORT),
+      port: Number(env.VITE_PORTAL_FRONT_PORT),
       watch: {
         ignored: ['**/tests/**']
       }
     },
     publicDir: 'public',
-    build: {
-      chunkSizeWarningLimit: 4000, // KB
-      sourcemap: false,
-      rollupOptions: {
-        output: {
-          manualChunks(id: string) {
-            if (!id.includes('node_modules')) {
-              return;
-            }
-            const pkg = Object.keys(dependencies).find(dep => id.includes(`/node_modules/${dep}/`));
-            if (!pkg || inlinePackages.includes(pkg)) {
-              return;
-            }
-            return pkg;
-          }
-        }
-      }
-    },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, './src')
+        '@': fileURLToPath(new URL('./src', import.meta.url))
       }
     },
-    optimizeDeps: {
-      exclude: ['react-scan']
+    build: {
+      chunkSizeWarningLimit: 2000, // KB
+      target: 'es2022',
+      sourcemap: false
     }
   });
 };
-
-// ======== Internals =======
-function muteWarningsPlugin(warningsToIgnore: string[][]): PluginOption {
-  const mutedMessages = new Set();
-  return {
-    name: 'mute-warnings',
-    enforce: 'pre',
-    config: userConfig => ({
-      build: {
-        rollupOptions: {
-          onwarn(warning, defaultHandler) {
-            if (warning.code) {
-              const muted = warningsToIgnore.find(
-                ([code, message]) => code == warning.code && warning.message.includes(message)
-              );
-
-              if (muted) {
-                mutedMessages.add(muted.join());
-                return;
-              }
-            }
-            defaultHandler(warning);
-          }
-        }
-      }
-    }),
-    closeBundle() {
-      const diff = warningsToIgnore.filter(x => !mutedMessages.has(x.join()));
-      if (diff.length > 0) {
-        this.warn('Some of your muted warnings never appeared during the build process:');
-        diff.forEach(m => this.warn(`- ${m.join(': ')}`));
-      }
-    }
-  };
-}
