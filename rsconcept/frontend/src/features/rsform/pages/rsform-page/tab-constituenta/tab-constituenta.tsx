0322d25bdf5d067c71765e08ff892c242c4e4aca
Ivan <8611739+IRBorisov@users.noreply.github.com>
2026-07-15 17:18:11 +0300

    M: Small fixes and ci bump

--- rsconcept/frontend/src/features/rsform/pages/rsform-page/tab-constituenta/tab-constituenta.tsx
+++ rsconcept/frontend/src/features/rsform/pages/rsform-page/tab-constituenta/tab-constituenta.tsx
@@ -149,8 +149,20 @@ export function TabConstituenta({ engine }: TabConstituentaProps) {
         disabled={disabled}
       />
 
-      <div className='flex min-h-0 flex-1 flex-col overflow-hidden pt-8 md:items-center lg:flex-row lg:items-stretch'>
-        <div className='mx-0 flex h-full min-h-0 min-w-120 flex-1 flex-col overflow-hidden pt-8 md:mx-auto md:w-195 xs:pt-0 lg:flex-none'>
+      <div
+        className={clsx(
+          'flex flex-col overflow-hidden md:items-center lg:flex-row lg:items-stretch',
+          'min-h-0 flex-1',
+          'pt-8'
+        )}
+      >
+        <div
+          className={clsx(
+            'flex flex-col overflow-hidden',
+            'h-full min-h-0 min-w-120 flex-1 md:w-195 lg:flex-none',
+            'mx-0 pt-8 md:mx-auto xs:pt-0'
+          )}
+        >
           <ConstituentsNarrowPicker
             className='min-h-0 flex-1'
             scrollChildren
